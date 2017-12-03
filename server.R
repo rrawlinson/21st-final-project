e1e01234c155e312f4e7e71df5249becdc9f3506
dtsullivan <Daniel@Daniels-Air.agd.com>
2017-12-03 14:44:01 -0800

    commenting

--- server.R
+++ server.R
@@ -1,3 +1,10 @@
+# Info 201b final project
+# Project: Gentri-Track
+# Group: 21st Ave
+
+# R server for shiny app
+# Creates data table of demographic information and locaiton
+
 my.server <- function(input, output) {}
 
 shinyServer(my.server)
--- ui.R
+++ ui.R
@@ -1,3 +1,10 @@
+# Info 201b final project
+# Project: Gentri-Track
+# Group: 21st Ave
+
+# R ui interface for shiny app
+# Generates html webpage with map plot and interactive labels
+
 my.ui <- fluidPage()
 
 
