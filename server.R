56d5ee98f3ab5aeb5fc585db306a7ac8057a0c59
dtsullivan <Daniel@D-128-208-212-91.dhcp4.washington.edu>
2017-12-04 16:21:51 -0800

    got api working

--- server.R
+++ server.R
@@ -6,20 +6,62 @@
 # Creates data table of demographic information and locaiton
 library("dplyr")
 library("httr")
+library("jsonlite")
 
 source("key.R")
-first.base.url <- "api.census.gov/data/"
-year <- "2016"
-second.base.url <- "/acs/acs1/profile"
-query <- first.base.url + year + second.base.url + "?"
-key <- "key=" + census.key
-data <- "get=" + "NAME"
-area <- "for=" + "state:01"
-full.url <- query + data + "&" + area + "&" + area
 
-print(full.url)
+# seattle zip codes
+# some of these don't have data for this table so we can
+# remove those zip codes
+zipcode.list <- c(98101, 98102, 98103, 98104, 98105, 98106,
+                  98107, 98108, 98109, 98111, 98112, 98113, 
+                  98114, 98115, 98116, 98117, 98118, 98119,
+                  98121, 98122, 98124, 98125, 98126, 98127,
+                  98129, 98131, 98132, 98133, 98134, 98136, 
+                  98139, 98141, 98144, 98145, 98146, 98154, 
+                  98161, 98164, 98165, 98170, 98174, 98175, 
+                  98177, 98178, 98181, 98185, 98190, 98191, 
+                  98195, 98199
+)
 
-df <- GET()
+ethnicity.list <- c(
+  "DP05_0058E",  # Total population
+  "DP05_0058PE", # Total percent
+  "DP05_0059E",  # White population
+  "DP05_0059PE", # white percent
+  "DP05_0060E",  # Black or African American population
+  "DP05_0060PE", # Black or African American percent
+  "DP05_0061E",  # American Indian and Alaska Native population
+  "DP05_0061PE", # American Indian and Alaska Native percent
+  "DP05_0062E",  # Asian population
+  "DP05_0062PE", # Asian percent
+  "DP05_0063E",  # Native Hawaiian and Other Pacific Islander population
+  "DP05_0063PE", # Native Hawaiian and Other Pacific Islander percent
+  "DP05_0064E",  # Some other race population
+  "DP05_0064PE"  # Some other race percent
+)
+
+zipcodes.param <- paste(zipcode.list, collapse = ",")
+ethnicity.param <- paste(ethnicity.list, collapse = ",")
+
+get.data.for.year <- function(year) { # year between 2012 and 2015
+  first.base.url <- "https://api.census.gov/data/"
+  year <- year  #"2015" for default
+  second.base.url <- "/acs5/profile"
+  query <- paste0(first.base.url, year, second.base.url, "?")
+  key <- paste0("key=", census.key)
+  data <- paste0("get=", ethnicity.param)
+  area <- paste0("for=", "zip%20code%20tabulation%20area:", zipcodes.param)
+  full.url <- paste0(query, data, "&", area, "&", key)
+  
+  # print(full.url)
+  df <- as.data.frame(fromJSON(full.url))
+  # print(is.data.frame(df))
+  names(df) <- as.matrix(df[1, ])
+  df <- df[-1, ]
+  # df[] <- lapply(df, function(x) type.convert(as.character(x))) # turns number values into characters
+  return(df)
+}
 
 
 
--- test.R
+++ test.R
@@ -2,20 +2,57 @@
 
 library("dplyr")
 library("httr")
-library("curl")
 library("jsonlite")
 
 source("key.R")
+
+# seattle zip codes
+# some of these don't have data for this table so we can
+# remove those zip codes
+zipcode.list <- c(98101, 98102, 98103, 98104, 98105, 98106,
+          98107, 98108, 98109, 98111, 98112, 98113, 
+          98114, 98115, 98116, 98117, 98118, 98119,
+          98121, 98122, 98124, 98125, 98126, 98127,
+          98129, 98131, 98132, 98133, 98134, 98136, 
+          98139, 98141, 98144, 98145, 98146, 98154, 
+          98161, 98164, 98165, 98170, 98174, 98175, 
+          98177, 98178, 98181, 98185, 98190, 98191, 
+          98195, 98199
+)
+
+ethnicity.list <- c(
+  "DP05_0058E",  # Total population
+  "DP05_0058PE", # Total percent
+  "DP05_0059E",  # White population
+  "DP05_0059PE", # white percent
+  "DP05_0060E",  # Black or African American population
+  "DP05_0060PE", # Black or African American percent
+  "DP05_0061E",  # American Indian and Alaska Native population
+  "DP05_0061PE", # American Indian and Alaska Native percent
+  "DP05_0062E",  # Asian population
+  "DP05_0062PE", # Asian percent
+  "DP05_0063E",  # Native Hawaiian and Other Pacific Islander population
+  "DP05_0063PE", # Native Hawaiian and Other Pacific Islander percent
+  "DP05_0064E",  # Some other race population
+  "DP05_0064PE"  # Some other race percent
+)
+
+zipcodes.param <- paste(zipcode.list, collapse = ",")
+ethnicity.param <- paste(ethnicity.list, collapse = ",")
+
+
 first.base.url <- "https://api.census.gov/data/"
-year <- "2016"
-second.base.url <- "/acs/acs1/profile"
+year <- "2015"
+second.base.url <- "/acs5/profile"
 query <- paste0(first.base.url, year, second.base.url, "?")
 key <- paste0("key=", census.key)
-data <- paste0("get=", "NAME")
-area <- paste0("for=", "state:*")
-full.url <- paste0(query, data, "&", area, "&", area, "&", key)
+data <- paste0("get=", ethnicity.param)
+area <- paste0("for=", "zip%20code%20tabulation%20area:", zipcodes.param)
+full.url <- paste0(query, data, "&", area, "&", key)
 
-print(full.url)
-
-df <- (GET(full.url))
-df1 <- fromJSON(content(df, "text"), header = TRUE)
+# print(full.url)
+df <- as.data.frame(fromJSON(full.url))
+# print(is.data.frame(df))
+names(df) <- as.matrix(df[1, ])
+df <- df[-1, ]
+# df[] <- lapply(df, function(x) type.convert(as.character(x))) # turns number values into characters
