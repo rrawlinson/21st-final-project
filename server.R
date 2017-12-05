681983318d36b3d5946e1e3545174c67bf850314
Autumn Derr <autumd@uw.edu>
2017-12-05 13:47:26 -0800

    changed orientation

--- server.R
+++ server.R
@@ -16,8 +16,16 @@ my.server <- function(input, output) {
     
     mydata12 <- get.data.for.year(input$year)
     
+    #race <- input$select
+    #num <- input$choices
+    #column <- paste(race, num)
+    
     plot <- ggplot(mydata12) +
-            geom_bar(mapping = aes(x = region, y = White_population, fill = region), stat = "identity") 
+      geom_bar(mapping = aes(x = region, y = input$select, fill = region), stat = "identity") +
+      coord_flip() +
+      labs(title = "Race Population and Zip Codes",  # plot title
+           x = "Zip Codes",  # x-axis label (with units!)
+           y = input$select)
     return(plot)
 
   })
