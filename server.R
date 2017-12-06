db32d35235e2b5d2822a3eeb45a796fe3320dcdb
Ruby Rawlinson <ruby22@uw.edu>
2017-12-06 00:27:50 -0800

    changed map size

--- server.R
+++ server.R
@@ -6,37 +6,40 @@
 # Creates data table of demographic information and locaiton
 
 source("apiData.R")
-library(choroplethrZip)
+library("choroplethrZip")
 library("stringr")
+library("ggmap")
+
 
 my.server <- function(input, output) {
   
-  map.df <- reactive({
-    return(get.data.for.year(input$year)) # example: "2015"
-  })
+   map.df <- reactive({
+     return(get.data.for.year(input$year)) # example: "2015"
+   })
   
-  output$map <- renderPlot({
-    ####################################################
-    # get info for map
-    df <- map.df()
-    
-    race <- input$race # example: "White_"
-    data.type <- input$data.type # example: "population"
-    
-    map.data <- df %>%  select(zip.code, eval(paste0(race, data.type)))
-    names(map.data) <- c("region", "value")
-    # map.data <- map.data %>% transform(value = as.numeric(value))
-    map.data$region <- as.character(as.factor(map.data$region)) # change zip codes to characters
-    map.data$value <- as.numeric(as.character(map.data$value)) # change values to numeric
-    map.data <- map.data %>% arrange(value) # arrange by value
-    
-    title.name <- str_replace_all(race, "_", " ")
-    ####################################################
-                       return(zip_choropleth(map.data,
-                           zip_zoom = zipcode.list,
-                           title    = paste0("Seattle ", title.name, "Ethnic Information by Zip Code"),
-                           legend   = data.type))
-  })
+   output$map <- renderPlot({
+     ####################################################
+     # get info for map
+     df <- map.df()
+  
+     race <- input$race # example: "White_"
+     data.type <- input$data.type # example: "population"
+  
+     map.data <- df %>%  select(zip.code, eval(paste0(race, data.type)))
+     names(map.data) <- c("region", "value")
+     # map.data <- map.data %>% transform(value = as.numeric(value))
+     map.data$region <- as.character(as.factor(map.data$region)) # change zip codes to characters
+     map.data$value <- as.numeric(as.character(map.data$value)) # change values to numeric
+     map.data <- map.data %>% arrange(value) # arrange by value
+  
+     title.name <- str_replace_all(race, "_", " ")
+     ####################################################
+                        return(zip_choropleth(map.data,
+                            zip_zoom = zipcode.list,
+                            title    = paste0("Seattle ", title.name, "Ethnic Information by Zip Code"),
+                            legend   = data.type
+                              ))
+   })
 }
 
 # my.server <- function(input, output) {
